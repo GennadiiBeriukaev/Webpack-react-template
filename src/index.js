0b09ee5b3ec48752cd93aec466fab0ea8cff91a1
GennadiiBeriukaev <gelany89@yandex.ru>
2023-02-18 20:17:05 +0500

    added webpack-config__template-html

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+function component(text) {
+    const element = document.createElement('h1');
+    element.textContent = text;
+    return element;
+  }
+  
+  document.body.prepend(component('Gena Webpack'));
